9189e3b4fcf888562b53441510f38781f1f63e3c
Ceaseless04 <kristian120304@gmail.com>
2023-08-03 23:52:44 -0400

    completed all 7 challenges, with a few styling tweaks at the end

--- index.js
+++ index.js
@@ -178,7 +178,18 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
-
+const [first_game, second_game, ...rest] = sortedGames;
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const top_game = document.createElement("p");
+top_game.innerHTML = `
+    ${first_game.name}
+`;
 
+firstGameContainer.append(top_game);
 // do the same for the runner up item
+const runner_up = document.createElement("p");
+runner_up.innerHTML = `
+    ${second_game.name}
+`;
+
+secondGameContainer.append(runner_up);
